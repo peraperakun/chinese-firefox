88309b34574cb2567db8f888bef273d7dc7a5023
Perapera Admin <peraperakun@gmail.com>
2015-06-15 13:34:46 +0900

    initial commit

---
+++ defaults/preferences/cperapera.js
@@ -0,0 +1,65 @@
+pref('extensions.cperapera.firstrun1', true);
+
+pref('extensions.cperapera.css', 'charcoal');
+pref('extensions.cperapera.cpick1', '#F2777A');
+pref('extensions.cperapera.cpick2', '#99CC99');
+pref('extensions.cperapera.cpick3', '#6699CC');
+pref('extensions.cperapera.cpick4', '#CC99CC');
+pref('extensions.cperapera.cpick5', '#CCCCCC');
+pref('extensions.cperapera.cpinyin', '#66CCCC');
+
+pref('extensions.cperapera.enmode', 1);
+pref('extensions.cperapera.highlight', true);
+pref('extensions.cperapera.selinlb', true);
+pref('extensions.cperapera.bottomlb', false);
+pref('extensions.cperapera.resizedoc', false);
+pref('extensions.cperapera.sticon', true);
+pref('extensions.cperapera.minihelp', true);
+pref('extensions.cperapera.flag', 'pera');
+pref('extensions.cperapera.showhanzi', 'boths');
+pref('extensions.cperapera.docolors', true);
+pref('extensions.cperapera.pinyin', 'tonemarks');
+pref('extensions.cperapera.outformat', 'edict');
+
+pref('extensions.cperapera.cmtoggle', true);
+pref('extensions.cperapera.cmlbar', false);
+pref('extensions.cperapera.tmtoggle', true);
+pref('extensions.cperapera.tmlbar', true);
+
+pref('extensions.cperapera.nopopkeys', false);
+
+pref('extensions.cperapera.toggle.key', 'INSERT');
+pref('extensions.cperapera.toggle.mod', 'alt');
+
+pref('extensions.cperapera.lbar.key', 'DELETE');
+pref('extensions.cperapera.lbar.mod', 'alt');
+
+pref('extensions.cperapera.dpriority', '');
+pref('extensions.cperapera.wpos', true);
+pref('extensions.cperapera.wpop', true);
+pref('extensions.cperapera.wmax', 10);
+pref('extensions.cperapera.namax', 20);
+
+pref('extensions.cperapera.kindex', 'COMP,H,L,E,DK,N,V,Y,P,IN,I,U');
+
+pref('extensions.cperapera.sfile', '');
+pref('extensions.cperapera.sfcs', 'utf-8');
+pref('extensions.cperapera.smaxfe', 1);
+pref('extensions.cperapera.smaxfk', 1);
+pref('extensions.cperapera.smaxce', 7);
+pref('extensions.cperapera.smaxck', 1);
+pref('extensions.cperapera.snlf', 1);
+pref('extensions.cperapera.ssep', 'Tab');
+
+pref('extensions.cperapera.version', '');
+pref('extensions.cperapera.checkversion', false);
+
+pref('extensions.cperapera.popdelay', 50);
+pref('extensions.cperapera.title', true);
+pref('extensions.cperapera.dpriority', '');
+pref('extensions.cperapera.hidedef', false);
+pref('extensions.cperapera.hidex', false);
+//	pref('extensions.cperapera.sticky', false);
+
+pref('extensions.cperapera.firsticon', true);
+pref('extensions.cperapera.show_warning_dlg', true);
